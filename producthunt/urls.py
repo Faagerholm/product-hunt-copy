1c29d35ca94764290c56091ff60ad20371502fab
Jimmy Fagerholm <fagerholm.jimmy@gmail.com>
2018-04-02 21:36:13 +0300

    accounts/signup now works and registers user to site.

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from products.views import home
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('accounts/', include('accounts.urls')),
 ]
