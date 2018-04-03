6ccb08183925e5c70bb9082afe1dc8aaf7a4359e
Jimmy Fagerholm <fagerholm.jimmy@gmail.com>
2018-04-03 09:16:46 +0300

    start on ability to add new product.

--- products/models.py
+++ products/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
-    url = models.TextField()
     pub_date = models.DateTimeField()
+    body = models.TextField()
+    url = models.TextField()
     votes_total = models.IntegerField(default=1)
     image = models.ImageField(upload_to='images/')
     icon = models.ImageField(upload_to='images/')
-    body = models.TextField()
     hunter = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def pub_date_pretty(self):
--- products/views.py
+++ products/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, 'products/home.html')
+
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.POST['icon'] and request.POST['image']:
+
+        else:
+            return render(request, 'products/create.html', {'error': 'All fields are required'})
+    else:
+        return render(request, 'products/create.html')
+
